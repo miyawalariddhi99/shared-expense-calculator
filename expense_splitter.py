ef2d443aa43d326050304a7438b603b01ec89f27
miyawalariddhi99 <riddhimiyawala99@gmail.com>
2025-12-11 16:27:32 +0530

    expense_splitter.py
    Removed example output from the script.

--- expense_splitter.py
+++ expense_splitter.py
@@ -14,11 +14,3 @@ amount_per_person = total_monthly_expense // number_of_persons
 print("Total amount you've to pay is:", amount_per_person)
 print("Total amount you've to pay for electricity is:", total_electricity_cost)
 
-##----------------------Example Output--------------------------------------
-Enter the total monthly rent: 30000
-Enter the total monthly food/groceries cost: 6000
-Enter the electricity units consumed: 300
-Enter the electricity charge per unit: 6
-Enter the number of persons sharing the room/flat: 5
-Total amount you've to pay is: 7560
-Total amount you've to pay for electricity is: 1800
